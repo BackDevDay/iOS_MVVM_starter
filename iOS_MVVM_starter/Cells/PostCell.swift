e51d274e7391a725d0a64cdff5d640c96d554591
keadtijarupat github <keadtijarupat.developers@gmail.com>
2021-03-18 11:44:27 +0700

    set cell & info

---
+++ iOS_MVVM_starter/Cells/PostCell.swift
@@ -0,0 +1,16 @@
+//
+//  PostCell.swift
+//  iOS_MVVM_starter
+//
+//  Created by เกียรติจารุพัฒน์ สมศรี on 18/3/2564 BE.
+//
+
+import Foundation
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLb: UILabel!
+    @IBOutlet weak var bodyLb: UILabel!
+    
+}
